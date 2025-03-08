88f53295092e1b94b0d2255cc5e98941cdaa98b7
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-03-08 16:31:17 +0100

    HomePage completed

--- src/Components/HouseWithBackyard.jsx
+++ src/Components/HouseWithBackyard.jsx
@@ -5,11 +5,11 @@ import { data } from '../constant/data';
 const HouseWithBackyard = () => {
     const HouseWithBackyardItems = data.slice(0, 4);
   return (
-    <div className="wrapper pt-6">
+    <div className="wrapper pt-6 px-4 lg:px-0">
     <h2 className="font-medium text-[1.5rem] leading-[36px] text-primary pb-4">
     HouseWithBackyard
     </h2>
-    <div className="gap-5 grid md:grid-cols-4 grid-cols-2">
+    <div className="gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ">
         {HouseWithBackyardItems.map((data) => (
             <ListItems key={data.id} {...data} />
         ))}
